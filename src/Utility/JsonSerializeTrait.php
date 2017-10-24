6cc279018a714fedb009a00b57ad1e474edb412e
root <root@EasyProfit2.Inomasrv01>
2017-10-24 17:44:19 +0200

    direct json encode in jsonserialize trait

--- src/Utility/JsonSerializeTrait.php
+++ src/Utility/JsonSerializeTrait.php
@@ -16,14 +16,13 @@
                     $newName = $prop->getName();
                     $newName = $newName[0] == '_'?substr($newName, 1):$newName;
                     $prop->setAccessible(true);
-                    $return[$newName] = $prop->getValue($this);
+                    $return[$newName] = json_encode($prop->getValue($this));
                 }
             }
             else {
-                return ['asd' => 1];
                 foreach($this->_expose as $prop) {
                     $newName = $prop[0] == '_'?substr($prop, 1):$prop;
-                    $return[$newName] = $this->{$prop};
+                    $return[$newName] = json_encode($this->{$prop});
                 }
             }
             return $return;
